c111983e496bf2d1135280132ff152ec8271fc03
rsgrava <rsgrava@hotmail.com>
2023-10-18 13:42:53 -0300

    player: add camera that follows player around

---
+++ Entities/player_camera.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+func _ready():
+	var map = get_tree().root.get_child(0).get_node("Background")
+	var rect = map.get_used_rect()
+	var size = map.get_tileset().tile_size
+	
+	limit_left = rect.position.x * size.x
+	limit_right = rect.end.x * size.x
+	limit_top = rect.position.y * size.y
+	limit_bottom = rect.end.y * size.y
+
+	var view_w = get_viewport().size.x
+	var view_h = get_viewport().size.y
+	if limit_right < view_w:
+		limit_right -= (limit_right - limit_left) / 2
+		limit_right += view_w / 2
+	if limit_bottom < view_h:
+		print(limit_top)
+		limit_top += (limit_bottom - limit_top) / 2
+		print(limit_top)
+		limit_top -= view_h / 2
+		print(limit_top)
